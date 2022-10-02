f74fab888c2c253fcec8ef24b6acd20d32895580
Fabriciolk <fabriciokashino@live.com>
2022-10-02 16:28:08 -0300

    Deleted unnecessary classes

--- src/main/java/com/gpti/bytego/ToDeleteLater/IPersonController.java
+++
@@ -1,6 +0,0 @@
-package com.gpti.bytego.ToDeleteLater;
-
-public interface IPersonController
-{
-    double getBMI(String personFullName);
-}
--- src/main/java/com/gpti/bytego/network/RequestHandler.java
+++
@@ -1,6 +0,0 @@
-package com.gpti.bytego.network;
-
-public class RequestHandler
-{
-
-}
